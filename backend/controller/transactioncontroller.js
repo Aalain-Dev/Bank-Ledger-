badbd5c2dcb5ec9c80b21072c0850760ffce0f37
Aalain-Bizdateup-Dev <aalain@bizdateup.com>
2026-03-04 06:56:19 +0530

    Created Half CreateTransaction controller

--- backend/controller/transactioncontroller.js
+++ backend/controller/transactioncontroller.js
@@ -1,8 +1,49 @@
+const Account = require("../models/account.model");
+const Transaction = require("../models/transaction.model");
 const transactionschema = require("../models/transaction.model");
 
-const createTransaction = () => {
+// Create a new Transaction
+// 1. validate the request body
+// 2. validate idempotency key
+// 3. check account status
+
+const createTransaction = async (req, res) => {
+  const { fromAccount, toAccount, amount, idempotencyKey } = req.body;
+  if (!fromAccount || !toAccount || !amount || !idempotencyKey) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
   try {
-  } catch (e) {}
+    // 1. validate the request body
+    const [isfromAccountidexsist, istoAccountidexsist] = await Promise.all([
+      Account.findOne({ _id: fromAccount }),
+      Account.findOne({ _id: toAccount }),
+    ]);
+    if (!isfromAccountidexsist || !istoAccountidexsist) {
+      return res.status(404).json({ message: "Account not found" });
+    }
+    // 2. validate idempotency key
+    const isidempotencyKeyExist = await Transaction.findOne({ idempotencyKey });
+
+    if (isidempotencyKeyExist) {
+      const response = {
+        Pending: { code: 200, message: "Transaction is pending" },
+        Completed: { code: 200, message: "Transaction completed" },
+        Failed: { code: 400, message: "Transaction failed" },
+        Reversed: { code: 200, message: "Transaction reversed" },
+      };
+      const { status } = isidempotencyKeyExist;
+      return res
+        .status(response[status].code)
+        .json({ message: response[status].message });
+    }
+    // 3. check account status
+    if (isfromAccountidexsist.status !== "Active" || isfromAccountidexsist.status !== "Active") {
+      return res.status(400).json({ message: "Account is not active" });
+    }
+    
+  } catch (e) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 module.exports = { createTransaction };
--- backend/routes/transaction.routes.js
+++ backend/routes/transaction.routes.js
@@ -1,7 +1,8 @@
 const { createTransaction } = require("../controller/transactioncontroller");
+const authMiddleware = require("../middleware/auth.middleware");
 
 const route = require("express").Router();
 
-route.post("/create", createTransaction);
+route.post("/create",authMiddleware, createTransaction);
 
 module.exports = route;
